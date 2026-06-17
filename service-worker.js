40cc6cb3d113380c84b551dd62fd0dfc3f54b58b
Luan <luandutra27@gmail.com>
2026-06-17 18:50:36 -0300

    chore: bump app cache for permissions release

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pdv-v52';
+const CACHE_NAME = 'pdv-v53';
 const STATIC_ASSETS = [
   './',
   './index.html',
@@ -15,7 +15,7 @@ const STATIC_ASSETS = [
   './src/styles/mobile.css',
   './src/config/runtime-config.js',
   './src/modules/auth/login.module.js',
-  './src/app.js?v=20260616-05'
+  './src/app.js?v=20260617-01'
 ];
 
 self.addEventListener('install', (event) => {
